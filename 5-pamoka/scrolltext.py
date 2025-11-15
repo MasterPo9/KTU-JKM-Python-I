f4ba435afc99a3a73a573b8017dd8dac8a06c241
MasterPo9 <damanskaspovilas0@gmail.com>
2025-11-15 12:00:55 +0200

    Refactor scrolltext.py to include scrolling text animation and utility functions

--- 5-pamoka/scrolltext.py
+++ 5-pamoka/scrolltext.py
@@ -3,66 +3,7 @@ A simple scrolling text animation in the console.
 Imports: time, os
 Function: scroll(text, color='0F', speed=0.001)
 may contain other useful functions"""
-list_of_all_letters =[chr(i) for i in range(32, 33)] + [chr(46)] + [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
-def scroll(text, color='0F', speed=0.001):
-    import time
-    import os
-    os.system(f'color {color}')
-
-    if text.__contains__("[]\{\}()<>/\\'\;:|`~!@#$%^&*-+=_1234567890,"):
-        print("Warning: The input text contains special characters that will not be animated")
-        time.sleep(2)
-    x=""
-    start=time.time()
-    for i in range(0, len(text)):
-        for a in range(0, len(list_of_all_letters)):
-            print(x+list_of_all_letters[a])
-            if text[i]==list_of_all_letters[a]:
-                break
-            time.sleep(speed)
-        x+=text[i]
-        if text==x:
-                break
-    print(x)
-def tetr(x, n=2, r=2):
-    # basicly tetration function, Tetration is repeated exponentiation.
-    # n -- the power, r -- the number of times to repeat exponentiation
-    if n == 0:
-        return 1
-    elif n == 1:
-        return x
-    else:
-        for i in range(r):
-            x **= n
-
-def printif(expr, msg=""):
-    if expr:
-        print(msg)
-
-def input_list(prompt="", separator=","):
-    return input(prompt).split(separator)
-
-def random_text(length=10):
-    import random
-    text=""
-    for i in range(length):
-        text+=list_of_all_letters[int(random.uniform(0, len(list_of_all_letters)))]
-    return text
-
-def random_color():
-    import random
-    hex_chars="0123456789ABCDEF"
-    color=""
-    for i in range(2):
-        color+=hex_chars[int(random.uniform(0, len(hex_chars)))]
-    return color
-
-def random_num_list(length=5, min_val=0, max_val=100):
-    import random
-    num_list=[]
-    for i in range(length):
-        num_list.append(int(random.uniform(min_val, max_val)))
-    return num_list
+#no find other functions in my github: https://github.com/MasterPo9/KTU-JKM-Python-I
 def cinput(a="Input: ", contains_sp=False, contains_cm=False, contains_both=False):
     it_paz=input(f"{a}")
 
