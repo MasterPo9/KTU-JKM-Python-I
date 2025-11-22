d6aef39033a4bd788b7a014d404c7d59e2cb1295
MasterPo9 <DamanskasPovilas0@gmail.com>
2025-11-22 10:03:00 +0200

    Enhance scroll function to support single line mode for text animation

--- scrolltext.py
+++ scrolltext.py
@@ -4,19 +4,19 @@ Imports: time, os
 Function: scroll(text, color='0F', speed=0.001)
 may contain other useful functions"""
 list_of_all_letters =[chr(i) for i in range(32, 33)] + [chr(46)] + [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
-def scroll(text, color='0F', speed=0.001):
+def scroll(text, color='0F', speed=0.001, SINGLE_LINE_MODE = False):
     import time
     import os
     os.system(f'color {color}')
 
-    if text.__contains__("[]\{\}()<>/\\'\;:|`~!@#$%^&*-+=_1234567890,"):
-        print("Warning: The input text contains special characters that will not be animated")
-        time.sleep(2)
     x=""
     start=time.time()
     for i in range(0, len(text)):
         for a in range(0, len(list_of_all_letters)):
-            print(x+list_of_all_letters[a])
+            if SINGLE_LINE_MODE:
+                print(f"\r\x1b[K{x+list_of_all_letters[a]}")
+            else:
+                print(x+list_of_all_letters[a])
             if text[i]==list_of_all_letters[a]:
                 break
             time.sleep(speed)
