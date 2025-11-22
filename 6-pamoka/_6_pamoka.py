4bf606e55537409bd812860929fed95e63cd7379
MasterPo9 <DamanskasPovilas0@gmail.com>
2025-11-22 11:12:44 +0200

    naew

--- 6-pamoka/_6_pamoka.py
+++ 6-pamoka/_6_pamoka.py
@@ -1,3 +1,5 @@
+import scrolltext as st
+
 def ka():
     def dal(sk):
         sum = 0
@@ -7,5 +9,49 @@ def ka():
     aq=int(input("iveskite skaiciu "))
     w=dal(aq)
     print(w)
-ka()
+
+#mane uzkniso klausyt
+def uzd1():
+    znks_k = int(input("Iveskite zinksniu kieki iki mokyklos: "))
+    sprg, supj = 0, 0
+    for i in range(1, znks_k+1):
+        if i % 10 == 0:
+            supj+=1
+        elif i % 10 == 5:
+            sprg+=1
+    print(f"Suplojimu bus {supj}, spragtejimu - {sprg}")
+
+def uzd2():
+    tmp=st.cinput("Iveskite, kiek snaigiu nukrito per pirmaja sekunde ir kiek sekundziu snigo: ")
+    k, n=int(tmp[0]), int(tmp[1]) #n - kiek sekundziu snigo; k - kiek snaigiu iskrito pirma sekunde
+    s=0
+    for i in range(1, n+1):
+        s+=k
+        k*=2
+    print(s)
+
+def uzd3():
+    st_d=int(input("Parasykite kuria savaites diena prasidejo menuo: "))
+    msdl=[]
+    for i in range(1, 31+1):
+        msdl.append(st_d)
+        st_d+=1
+        if st_d > 7:
+            st_d=1
+    intervalas=st.cinput("Iveskite dienu intervala: ")
+    pr, end=int(intervalas[0]), int(intervalas[1])
+    for i in range(pr, end+1):
+        print(f"{i}-oji diena: {msdl[i-1]}")
+
+def uzd4():
+    kl=st.cinput("Iveskite kiek isrideno tasku , ant kiekvieno kauliuko: ", True)
+    max=len(kl)*6
+    surinko=sum(kl)
+    vid=surinko/len(kl)
+    if surinko <= max//2:
+        laimejo="Loterija pralaimeta"
+    else:
+        laimejo="Loterija laimeta"
+    print(f"Is viso buvo galima surinkti tasku: {max}\nTomas is viso surinko {surinko} tasku\nJo tasku vidurkis: {vid} Tasko\n{laimejo}")
+uzd4()
 
---
+++ 6-pamoka/scrolltext.py
@@ -0,0 +1,104 @@
+"""
+A simple scrolling text animation in the console.
+Imports: time, os
+Function: scroll(text, color='0F', speed=0.001)
+may contain other useful functions"""
+list_of_all_letters =[chr(i) for i in range(32, 33)] + [chr(46)] + [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
+def scroll(text, color='0F', speed=0.001, SINGLE_LINE_MODE = False):
+    import time
+    import os
+    os.system(f'color {color}')
+
+    x=""
+    start=time.time()
+    for i in range(0, len(text)):
+        for a in range(0, len(list_of_all_letters)):
+            if SINGLE_LINE_MODE:
+                print(f"\r\x1b[K{x+list_of_all_letters[a]}")
+            else:
+                print(x+list_of_all_letters[a])
+            if text[i]==list_of_all_letters[a]:
+                break
+            time.sleep(speed)
+        x+=text[i]
+        if text==x:
+                break
+    print(x)
+def tetr(x, n=2, r=2):
+    # basicly tetration function, Tetration is repeated exponentiation.
+    # n -- the power, r -- the number of times to repeat exponentiation
+    if n == 0:
+        return 1
+    elif n == 1:
+        return x
+    else:
+        for i in range(r):
+            x **= n
+
+def printif(expr, msg=""):
+    if expr:
+        print(msg)
+
+def input_list(prompt="", separator=","):
+    return input(prompt).split(separator)
+
+def random_text(length=10):
+    import random
+    text=""
+    for i in range(length):
+        text+=list_of_all_letters[int(random.uniform(0, len(list_of_all_letters)))]
+    return text
+
+def random_color():
+    import random
+    hex_chars="0123456789ABCDEF"
+    color=""
+    for i in range(2):
+        color+=hex_chars[int(random.uniform(0, len(hex_chars)))]
+    return color
+
+def random_num_list(length=5, min_val=0, max_val=100):
+    import random
+    num_list=[]
+    for i in range(length):
+        num_list.append(int(random.uniform(min_val, max_val)))
+    return num_list
+def cinput(a="Input: ", INTMODE=False, contains_sp=False, contains_cm=False, contains_both=False):
+    it_paz=input(f"{a}")
+
+    for i in range(0, len(it_paz)):
+        if it_paz[i] == " ":
+            contains_sp=True
+        if it_paz[i] == ",":
+            contains_cm=True                                #auto detect format (split by spaces, or commas)
+
+    if contains_cm and contains_sp: 
+        contains_both=True
+        contains_cm, contains_sp=False, False               #or if both are used, split by both
+
+    if contains_sp:
+        it=it_paz.split(" ")
+
+    if contains_cm:
+        it=it_paz.split(",")
+
+    if contains_both:
+        it=it_paz.split(", ")
+    if INTMODE: 
+        it_int=[]
+        for i in it:
+            it_int.append(int(i))
+        return it_int
+    else:
+        return it
+
+def lmax(a=[]):
+    kb=0
+    didzc=0
+    ct=1
+    for i in a:
+        kb+=float(i)
+        if float(i) >= didzc:
+            didzc=float(i)
+        ct+=1
+    return didzc
